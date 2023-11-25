b18882fe6050dbb1e4b9b73021fb0927ca20b1bd
Emmanuel Sampaio <emmanuellima07@alu.ufc.br>
2023-11-24 23:56:11 -0300

    adicionando estilização para tipo de pokemon

--- script.js
+++ script.js
@@ -4,19 +4,41 @@ function displayPokemon(pokemon) {
   const pokemonDiv = document.createElement('div');
   pokemonDiv.classList.add('pokemon');
 
+  const anchorElement = document.createElement('a');
+  anchorElement.href = `https://pokeapi.co/api/v2/pokemon/${pokemon.id}/`;
+
   const imageUrl = pokemon.sprites.front_default;
   const imgElement = document.createElement('img');
   imgElement.src = imageUrl;
   imgElement.classList.add('pokemon-image');
-  pokemonDiv.appendChild(imgElement);
+  anchorElement.appendChild(imgElement);
 
   const nameElement = document.createElement('p');
   nameElement.textContent = `Name: ${pokemon.name}`;
-  pokemonDiv.appendChild(nameElement);
+  anchorElement.appendChild(nameElement);
 
   const typeElement = document.createElement('p');
   typeElement.textContent = `Type: ${pokemon.types.map(type => type.type.name).join(', ')}`;
-  pokemonDiv.appendChild(typeElement);
+  anchorElement.appendChild(typeElement);
+
+ 
+  let isGrass = false;
+  pokemon.types.forEach(type => {
+    const typeClass = type.type.name.toLowerCase();
+    pokemonDiv.classList.add(typeClass);
+
+    
+    if (type.type.name === 'grass') {
+      isGrass = true;
+    }
+  });
+
+ 
+  if (isGrass) {
+    pokemonDiv.classList.remove('poison');
+  }
+
+  pokemonDiv.appendChild(anchorElement);
 
   return pokemonDiv;
 }
@@ -25,9 +47,9 @@ async function fetchAndDisplayAllPokemon(url) {
   const container = document.createElement('div');
   container.classList.add('pokemon-container');
 
-  let nextUrl = url;
-
   try {
+    let nextUrl = url;
+
     while (nextUrl) {
       const response = await fetch(nextUrl);
       if (!response.ok) {
@@ -40,8 +62,12 @@ async function fetchAndDisplayAllPokemon(url) {
       for (const pokemon of pokemons) {
         const pokemonResponse = await fetch(pokemon.url);
         const pokemonData = await pokemonResponse.json();
-        const pokemonElement = displayPokemon(pokemonData);
-        container.appendChild(pokemonElement);
+
+        
+        if (pokemonData.id <= 151) {
+          const pokemonElement = displayPokemon(pokemonData);
+          container.appendChild(pokemonElement);
+        }
       }
 
       nextUrl = data.next;
