9555fefc606fa60b5a95c088af54fc52f62c205a
Emmanuel Sampaio <emmanuellima07@alu.ufc.br>
2023-11-24 22:17:28 -0300

    adicionando os pokemons e estilização básica

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
-const pokemonId = 1;
+const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 
+function displayPokemon(pokemon) {
+  const pokemonDiv = document.createElement('div');
+  pokemonDiv.classList.add('pokemon');
 
-const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
+  const imageUrl = pokemon.sprites.front_default;
+  const imgElement = document.createElement('img');
+  imgElement.src = imageUrl;
+  imgElement.classList.add('pokemon-image');
+  pokemonDiv.appendChild(imgElement);
 
+  const nameElement = document.createElement('p');
+  nameElement.textContent = `Name: ${pokemon.name}`;
+  pokemonDiv.appendChild(nameElement);
 
-fetch(apiUrl)
-  .then(response => {
-    
-    if (!response.ok) {
-      throw new Error(`Erro na requisição: ${response.status}`);
+  const typeElement = document.createElement('p');
+  typeElement.textContent = `Type: ${pokemon.types.map(type => type.type.name).join(', ')}`;
+  pokemonDiv.appendChild(typeElement);
+
+  return pokemonDiv;
+}
+
+async function fetchAndDisplayAllPokemon(url) {
+  const container = document.createElement('div');
+  container.classList.add('pokemon-container');
+
+  let nextUrl = url;
+
+  try {
+    while (nextUrl) {
+      const response = await fetch(nextUrl);
+      if (!response.ok) {
+        throw new Error(`Erro na requisição: ${response.status}`);
+      }
+
+      const data = await response.json();
+      const pokemons = data.results;
+
+      for (const pokemon of pokemons) {
+        const pokemonResponse = await fetch(pokemon.url);
+        const pokemonData = await pokemonResponse.json();
+        const pokemonElement = displayPokemon(pokemonData);
+        container.appendChild(pokemonElement);
+      }
+
+      nextUrl = data.next;
     }
- 
-    return response.json();
-  })
-  .then(data => {
-   
-    console.log('Dados do Pokémon:', data);
-  })
-  .catch(error => {
-    
-    console.error('Erro:', error);
-  });
+
+    document.body.appendChild(container);
+  } catch (error) {
+    console.error('Erro ao carregar lista de Pokémon:', error);
+  }
+}
+
+
+fetchAndDisplayAllPokemon(apiUrl);
